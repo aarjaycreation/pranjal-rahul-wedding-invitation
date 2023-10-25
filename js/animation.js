f9a2fd1ad17bf5f13105308bf2c65daefb78c3e1
rahul <aarjayjangir@gmail.com>
2023-10-25 16:57:57 -0700

    commit on 25 oct 2023

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+const rahul = new SplitType('.rahul');
+
+gsap.to('.char',{
+ y:0,
+ stagger:0.05,
+ delay:5,
+ duration:1
+})
+
+
+
+
+// const tl = new TimelineMax();
+
+// tl
+// .from($('.text__l'), 1, { y: -50}, 0)
+// .from($('.text__m'), 2, { rotation: 45, transformOrigin: '170% 50%' }, '-=1.5')
+// .from($('.text__o'), 2, { y: '100%'}, 0)
+// .from($('.text__re'), 2, { x: '-100%'}, 0)
